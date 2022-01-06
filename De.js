2b0537de89ead09ba30c8695add52af3bab209dd
Abdoulk97 <93389769+Abdoulk97@users.noreply.github.com>
2022-01-06 16:13:16 +0100

    Create De.js

---
+++ De.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class De extends React.Component {
+
+  render() {
+    return (
+      <span className="De">
+          <img src= {this.props.face} className="App-logo" alt="dé" />
+      </span>
+    );
+  }
+}
+ 
+
+
+export default De;
